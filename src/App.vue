3f68c0e99fe1c88924d93fb0e705088d855d4bd7
Darman1987 <darman1987@hotmail.com>
2023-06-14 15:38:28 +1000

    slots

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,17 @@
     <h1>{{ title }}</h1>
     <p>Welcome...</p>
     <div v-if="showModal">
-      <Modal :heading="heading" :text="text" theme="sale" @close="toggleModal" />
-    </div>  
-    <button @click.alt="toggleModal">open modal (alt click)</button>
-  </div>
+      <Modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Givaway!</h1>
+        <p>Grab your givaway swag for half price!</p>
+      </Modal>
+  </div>  
+  <button @click.alt="toggleModal">open modal (alt click)</button>
+  </div> 
 </template>
 
 <script>
@@ -18,8 +25,6 @@ export default {
   data() {
     return {
       title: 'My First Vue App!',
-      heading: 'Sign up for the Givaway!',
-      text: 'Grab your ninja swag for half price!',
       showModal: false,
     }
   },
