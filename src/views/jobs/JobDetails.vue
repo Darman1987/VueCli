85d10081695ee88123fe147a759970443ddbf512
Darman1987 <darman1987@hotmail.com>
2023-06-16 11:42:37 +1000

    redirecta and No found page

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,21 +1,14 @@
 <template>
-    <div>
-        <h1>Job Details</h1>
-        <p>The job id is {{ id }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: ['id'],
-    // data() {
-    //   return {
-    //     id: this.$route.params.id
-    //   }
-    // }
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+  <h1>Job Details</h1>
+  <p>The job id is {{ id }}</p>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+}
+</script>
+
+<style>
+
+</style>
